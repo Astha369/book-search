2694a1eddf42de7acae3c01be0c79ce8a6aedb68
Astha369 <tripathiastha369@gmail.com>
2024-05-12 16:34:17 +0530

    second commit

--- src/BookList.js
+++ src/BookList.js
@@ -1,13 +1,28 @@
+// BookList.js
 import React from 'react';
+
+// BookList.js (continued)
+const openHighResImage = (book) => {
+  // Construct the URL for the high-resolution image
+  const highResImageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
+  
+  // Open the high-resolution image in a new window
+  window.open(highResImageUrl);
+};
+
 
 const BookList = ({ books, onBookSelect }) => {
   return (
     <div>
       {books.map((book) => (
         <div key={book.key} onClick={() => onBookSelect(book)}>
-          <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`} alt={book.title} />
+          <img
+            src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+            alt={book.title}
+            onClick={() => openHighResImage(book)}
+          />
           <h3>{book.title}</h3>
-          <p>by {book.author_name.join(', ')}</p>
+          <p>by {book.author_name ? book.author_name.join(', ') : 'Unknown Author'}</p>
         </div>
       ))}
     </div>
