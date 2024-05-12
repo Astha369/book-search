4975995ae6816edf03291169d310aec216fcc5fc
Astha369 <tripathiastha369@gmail.com>
2024-05-12 18:31:28 +0530

    fourth commit

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -4,10 +4,10 @@ const BookDetails = ({ book, onReturn }) => {
   return (
     <div>
       <h2>{book.title}</h2>
-      <p>by {book.author_name.join(', ')}</p>
-      <p>Published: {book.first_publish_year}</p>
-      <p>Pages: {book.number_of_pages}</p>
-      <p>Subjects: {book.subject.join(', ')}</p>
+      <p>by {book.author_name ? book.author_name.join(', ') : 'Unknown Author'}</p>
+      <p>Published: {book.first_publish_year || 'N/A'}</p>
+      <p>Pages: {book.number_of_pages || 'N/A'}</p>
+      <p>Subjects: {book.subject ? book.subject.join(', ') : 'N/A'}</p>
       <br></br>
       <button onClick={onReturn}>Back to Search Results</button>
     </div>
--- src/BookList.js
+++ src/BookList.js
@@ -1,12 +1,10 @@
-// BookList.js
 import React from 'react';
 
-// Function to open high-resolution image in a new window
 const openHighResImage = (book) => {
-  // Check if cover_i exists before constructing the URL
-  if (book.cover_i) {
+  if (book && book.cover_i) {
     // Construct the URL for the high-resolution image
     const highResImageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
+  
     // Open the high-resolution image in a new window
     window.open(highResImageUrl);
   }
@@ -16,23 +14,14 @@ const BookList = ({ books, onBookSelect }) => {
   return (
     <div>
       {books.map((book) => (
-        <div key={book.key} className="book-item" onClick={() => onBookSelect(book)}>
-          {/* Conditional rendering for cover image */}
-          {book.cover_i && (
-            <img
-              src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
-              alt={book.title}
-              onClick={() => openHighResImage(book)}
-            />
-          )}
-          <div className="book-details">
-            {/* Render title */}
-            <h2>{book.title}</h2>
-
-
-            {/* Conditional rendering for author name */}
-            <p>{book.author_name ? book.author_name.join(', ') : 'Unknown Author'}</p>
-          </div>
+        <div key={book.key} onClick={() => onBookSelect(book)}>
+          <img
+            src={book.cover_i ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` : ''}
+            alt={book.title}
+            onClick={() => openHighResImage(book)}
+          />
+          <h3>{book.title}</h3>
+          <p>by {book.author_name ? book.author_name.join(', ') : 'Unknown Author'}</p>
         </div>
       ))}
     </div>
